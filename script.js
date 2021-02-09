b9fd15142a6ced569e7ca576dd298f8a71593e8d
Sophie <sophie88leigh@gmail.com>
2021-02-09 13:31:34 -0500

    added case codes for green,yellow,purple,orange

--- script.js
+++ script.js
@@ -8,6 +8,18 @@ function pressed() {
         case "blue":
             document.getElementById("square").style.backgroundColor="blue";
             break;
+        case "green":
+            document.getElementById("square").style.backgroundColor="green";
+            break;
+        case "yellow":
+            document.getElementById("square").style.backgroundColor="yellow";
+            break;
+        case "purple":
+            document.getElementById("square").style.backgroundColor="purple";
+            break;
+        case "orange":
+            document.getElementById("square").style.backgroundColor="orange";
+            break;
         default: 
             document.getElementById("square").style.backgroundColor="white";
         break;
